fc694f2827e81fe916cddc39f2e0c898e2086f8b
Jesus Bellon Quixal <jbqsbd@gmail.com>
2019-02-02 10:26:03 +0100

    add users service

--- randomusersDemo/Network/Services/UserService.swift
+++ randomusersDemo/Network/Services/UserService.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import Moya
+import RxSwift
+import Result
+
+protocol UserService {
+    func getUsers(page: Int?, onSuccess: @escaping (([User]) -> Void), onError: @escaping ((Error) -> Void))
+}
+
+class RandomUserService: UserService {
+    let userProvider = MoyaProvider<UsersEndpoint>()
+    let disposeBag = DisposeBag.init()
+    
+    func getUsers(page: Int?, onSuccess: @escaping (([User]) -> Void), onError: @escaping ((Error) -> Void)) {
+        userProvider.rx.request(.users(page: page)).asObservable().subscribe(onNext: { (response) in
+            let users = try? JSONDecoder().decode(Users.self, from: response.data)
+            guard let result = users?.results else {
+                onSuccess([])
+                return
+            }
+            onSuccess(result)
+        }, onError: { (error) in
+            onError(error)
+        }, onCompleted: nil, onDisposed: nil).disposed(by: disposeBag)
+    }
+}
