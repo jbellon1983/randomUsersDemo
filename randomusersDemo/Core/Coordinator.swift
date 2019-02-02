ffceabbb0eb752cb07ae9e549d679c810e36b388
Jesus Bellon Quixal <jbqsbd@gmail.com>
2019-02-02 19:29:48 +0100

    add coordinator pattern

---
+++ randomusersDemo/Core/Coordinator.swift
@@ -0,0 +1,32 @@
+//
+//  Coordinator.swift
+//  randomusersDemo
+//
+//  Created by Jesus Bellon Quixal on 02/02/2019.
+//  Copyright © 2019 Jesus Bellon Quixal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+}
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = UsersView.view(viewModel: UsersViewModel.init(service: UserService()))
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
+
